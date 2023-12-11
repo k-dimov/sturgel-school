6d275825ce276ffb492278e9c66f9d89895e1395
k-dimov <kaloyan.dimov@hotmail.com>
2023-12-11 15:15:55 +0200

    Added Header component

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyB1jdy8jcegupBS8PfcVgY7puieA49q5RU",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
+export const auth = getAuth(app);
