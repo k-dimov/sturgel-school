2efbd4775aa551fb15105e8a06ece82c8a154fe2
k-dimov <kaloyan.dimov@hotmail.com>
2023-12-12 13:44:58 +0200

    Added some header styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,13 +4,17 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 
+import styles from './Header.module.css'
+
 function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Container className={styles.navContainer}>
+                <Navbar.Brand href="#home">
+                    <img src="/public/ela-logo.png" alt="ela" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className={styles.basicNavbar}>
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
@@ -29,8 +33,12 @@ function Header() {
                                 Separated link
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/register">
+                            Register
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
